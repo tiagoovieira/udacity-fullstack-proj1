936ebced79b2f8bc05b9365f10568ff6777e69bb
Tiago Vieira <tiagoovieira@gmail.com>
2015-02-10 13:23:52 +0000

    First version of the movies website

---
+++ README.md
@@ -0,0 +1,3 @@
+1. First Install Python (preferably, 2.7) in your computer, in case you don't already have it
+
+2. Then run either in the terminal (Linux/Mac) in this folder the command "python center.py" or using the IDLE 
---
+++ media.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+class Movie():
+	"""Stores relevant information about a movie, such as title, poster and trailer url """
+	def __name__(self):
+		return "Movie"
+
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+	def __init__(self, movie_title, movie_storyline, movie_poster_url, movie_youtube_url):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = movie_poster_url
+		self.trailer_youtube_url = movie_youtube_url
+		
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
