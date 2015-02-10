2803ad511c6968b1350e8a93654b5ca2eefaa7fd
Tiago Vieira <tiagoovieira@gmail.com>
2015-02-10 13:36:50 +0000

    Reorder media.py

--- media.py
+++ media.py
@@ -2,10 +2,12 @@ import webbrowser
 
 class Movie():
 	"""Stores relevant information about a movie, such as title, poster and trailer url """
+
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]	
+
 	def __name__(self):
 		return "Movie"
 
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 	def __init__(self, movie_title, movie_storyline, movie_poster_url, movie_youtube_url):
 		self.title = movie_title
 		self.storyline = movie_storyline
